b0cc684e77c355bfd485281b636b9061458a2934
Roman <romantimilsina@gmail.com>
2023-05-27 10:00:36 +0545

    Update_part

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import postRoutes from './routes/posts.js'
 import authRoutes from './routes/auth.js'
 import commentRoutes from './routes/comments.js'
 import likeRoutes from './routes/likes.js'
+import relationshipRoutes from './routes/relationships.js'
+
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import multer from "multer";
@@ -43,6 +45,8 @@ app.use("/api/posts", postRoutes)
 app.use("/api/auth", authRoutes)
 app.use("/api/comments", commentRoutes)
 app.use("/api/likes", likeRoutes)
+app.use("/api/relationships", relationshipRoutes)
+
 
 app.listen(8800, () => {
   console.log("API working!")
